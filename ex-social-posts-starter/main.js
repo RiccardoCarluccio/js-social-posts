8ab6ec981acab9cfe41060da34c99e0244d3d550
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-18 16:49:49 +0200

    v2.0 aggiunte quasi tutte le info personalizzate

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -56,7 +56,8 @@ const POSTS = [
     }
 ];
 
-//
+//ID 4 non ha un'immagine profilo
+//da fare la differenza di date e settare quando è stato creato il post
 
 POSTS.forEach(post => {
     const NEW_POST = document.createElement('div');
@@ -65,17 +66,17 @@ POSTS.forEach(post => {
             <div class="post__header">
                 <div class="post-meta">                    
                     <div class="post-meta__icon">
-                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                        <img class="profile-pic" src="${post.author.image}" alt="Phil Mangione">                    
                     </div>
                     <div class="post-meta__data">
-                        <div class="post-meta__author">Phil Mangione</div>
+                        <div class="post-meta__author">${post.author.name}</div>
                         <div class="post-meta__time">4 mesi fa</div>
                     </div>                    
                 </div>
             </div>
-            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__text">${post.content}</div>
             <div class="post__image">
-                <img src="https://unsplash.it/600/300?image=171" alt="">
+                <img src="${post.media}" alt="">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
@@ -86,7 +87,7 @@ POSTS.forEach(post => {
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
                     </div>
                 </div> 
             </div>            
